8ccc56be1ee23738d690767a83ffb3a2f9ba8dfa
Bogdan Rybak <boguzs@gmail.com>
2015-05-10 11:24:21 -0500

    Added click tracking and updated Megan's pic

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-/**
-* Function that tracks a click on an outbound link in Google Analytics.
-* This function takes a valid URL string as an argument, and uses that URL string
-* as the event label.
-*/
-var trackOutboundLink = function(url) {
-   ga('send', 'event', 'outbound', 'click', url, {'hitCallback':
-     function () {
-     document.location = url;
-     }
-   });
-}
+
+function hitBack(link, target) {
+  target ? window.open(link, target) : window.location.href = link;
+};
+
+$(document).ready(function(){
+
+	// track links with google
+	$('a.outbound').click(function(e) {
+		var link = $(this);
+		var trackingName = link.attr('data-eventName') || 'outbound';
+
+		ga('send', 'event', trackingName, 'click', link.attr('href'),
+			{'hitCallback': hitBack(link.attr('href'), link.attr('target') === '_blank') }
+		);
+		
+		e.preventDefault();
+	});
+
+});
